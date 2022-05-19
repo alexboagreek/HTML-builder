a13dcd093999433f07c4f71f3ea086d923d02ac5
Alex <olivesargreek@gmail.com>
2022-05-19 12:45:35 +0300

    fix: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,34 +1,43 @@
 const fs = require('fs');
-const path = require('path');
 const fsProm = require('fs/promises');
+const path = require('path');
 
-const pathCss = path.join(__dirname, 'styles');
-const pathBundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const pathToCss = path.join(__dirname, 'styles');
+const pathToBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 async function createBundle() {
-  const allFiles = await fsProm.readdir(pathCss);
+  const allFiles = await fsProm.readdir(pathToCss);
   const cssFiles = allFiles.filter(file => path.extname(file) === '.css');
-  const stream = fs.createWriteStream(pathBundle, 'utf8');
+  const stream = fs.createWriteStream(pathToBundle, 'utf8');
 
   streamMergeRecursive(cssFiles, stream);
-
 }
 
-function streamMergeRecursive(files = [], fileWriteStream){
+async function streamMergeRecursive(files = [], fileWriteStream) {
   if (!files.length) {
     return fileWriteStream.end();
   }
-  const currentFile = path.resolve(pathCss, files.shift());
-  const currentReadStream = fs.createReadStream(currentFile, 'utf8');
-  
-  currentReadStream.pipe(fileWriteStream, { end:false });
-  currentReadStream.on('end', function () {
+
+  const currentFile = path.resolve(pathToCss, files.shift());
+
+  const stat = await fsProm.stat(currentFile);
+
+  if (!stat.isDirectory()) {
+    const currentReadStream = fs.createReadStream(currentFile, 'utf8');
+
+    currentReadStream.pipe(fileWriteStream, { end: false });
+    currentReadStream.on('end', function () {
+      fileWriteStream.write('\n');
+      streamMergeRecursive(files, fileWriteStream);
+    });
+
+    currentReadStream.on('error', function (error) {
+      console.error(error);
+      fileWriteStream.close();
+    });
+  } else {
     streamMergeRecursive(files, fileWriteStream);
-  });
-  
-  currentReadStream.on('error', function (error) {
-    console.error(error);
-    fileWriteStream.close();
-  });
+  }
 }
+
 createBundle();
